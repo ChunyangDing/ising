567d1f83a299c3ea980c5d122301d86c20a1ab67
uysmeh <uysmeh@gmail.com>
2018-04-04 16:19:14 -0400

    Implemented annealing
    Implemented annealing, ignoring numsteps in the annealing function and decaying only for the duration of num_burnin

--- python_with_c/annealing.py
+++ python_with_c/annealing.py
@@ -1,8 +1,14 @@
 def T_anneal(T, ii, num_steps, num_burnin):
 
     #implement annealing code here
-
-    T_a = T
+	
+	T_start = 2
+	alpha = e ** ( log(T/T_start) / num_burnin )
+	
+	if ii < num_burnin:
+		T_a = T_start * alpha ** (ii)
+	else:
+		T_a = T
 
     return float(T_a)
 
@@ -10,6 +16,12 @@ def B_anneal(B, ii, num_steps, num_burnin):
 
     #implement annealing code here
     
-    B_a = B
-
+	B_start = 2
+	alpha = e ** ( log(B/B_start) / num_burnin )
+	
+	if ii < num_burnin:
+		B_a = B_start * alpha ** (ii)
+	else:
+		B_a = B
+	
     return float(B_a)
