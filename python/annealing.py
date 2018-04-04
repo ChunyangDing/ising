08edb0018bb3546f1069ced214175f7140d9749c
uysmeh <uysmeh@gmail.com>
2018-04-04 16:50:09 -0400

    small fixes
    more to come

--- python/annealing.py
+++ python/annealing.py
@@ -2,7 +2,7 @@ def T_anneal(T, ii, num_steps, num_burnin):
 
     #implement annealing code here
 	
-	T_start = 2
+	T_start = 10
 	alpha = e ** ( log(T/T_start) / num_burnin )
 	
 	if ii < num_burnin:
@@ -10,18 +10,17 @@ def T_anneal(T, ii, num_steps, num_burnin):
 	else:
 		T_a = T
 
-    return float(T_a)
+	return float(T_a)
 
 def B_anneal(B, ii, num_steps, num_burnin):
 
     #implement annealing code here
     
-	B_start = 2
+	B_start = 10
 	alpha = e ** ( log(B/B_start) / num_burnin )
 	
 	if ii < num_burnin:
 		B_a = B_start * alpha ** (ii)
 	else:
 		B_a = B
-	
-    return float(B_a)
+	return float(B_a)
--- python_with_c/annealing.py
+++ python_with_c/annealing.py
@@ -1,27 +1,28 @@
+import numpy as np
+
 def T_anneal(T, ii, num_steps, num_burnin):
 
     #implement annealing code here
-	
-	T_start = 2
-	alpha = e ** ( log(T/T_start) / num_burnin )
-	
-	if ii < num_burnin:
+	T_start = 10
+
+	if (num_burnin > 0) and (ii < num_burnin):
+		alpha = np.exp( np.log(T/T_start) / num_burnin )
 		T_a = T_start * alpha ** (ii)
 	else:
 		T_a = T
 
-    return float(T_a)
+	print(T_a)
+	return float(T_a)
 
 def B_anneal(B, ii, num_steps, num_burnin):
 
     #implement annealing code here
     
-	B_start = 2
-	alpha = e ** ( log(B/B_start) / num_burnin )
-	
-	if ii < num_burnin:
+	B_start = 10
+	if (num_burnin > 0) and (ii < num_burnin):
+		alpha = np.exp( np.log(B/B_start) / num_burnin )
 		B_a = B_start * alpha ** (ii)
 	else:
 		B_a = B
-	
-    return float(B_a)
+
+	return float(B_a)
